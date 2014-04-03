6d43fe0a1ebca74db12032394ae22103ba10c3f4
adam <adam.fazzari@gmail.com>
2014-04-02 21:54:38 -0400

    Added arm/disarm link to flask page

--- Glitch.py
+++ Glitch.py
@@ -10,7 +10,7 @@ from ArduinoClient import ArduinoClient
 import time
 import ConfigParser
 import os
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
 
 app = Flask(__name__)
